6d82ad9c59e57597ce12a005c244187bb4cee1f3
Peter A. Tariche <ptariche@gmail.com>
2016-12-29 14:25:12 -0800

    added additional checks for rest methods

--- src/controllers/person.rs
+++ src/controllers/person.rs
@@ -13,43 +13,72 @@ use lib;
 use models;
 use uuid::Uuid;
 
+
+fn validate_get_delete (u: &str) -> BTreeMap<String, Vec<String>> {
+  let mut validations: BTreeMap<String, Vec<String>> = BTreeMap::new();
+
+  validations.insert("uuid".to_string(), vec![u.to_string(), helpers::validator::ValidTypes::Uuid.to_string()]);
+  let result = helpers::validator::validate_map(validations);
+
+  result
+}
+
 pub fn get<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a> {
   use lib::schema::persons::dsl::{persons, uuid};
 
-  let connection = lib::db::establish_connection();
-
-  let input_uuid = Uuid::parse_str(req.param("uuid").unwrap()).unwrap();
-  let results = persons.filter( uuid.eq(input_uuid))
-    .load::<models::people::Person>(&connection)
-    .expect("error pulling person matching uuid");
+  let validation: BTreeMap<String, Vec<String>> = validate_get_delete(req.param("uuid").unwrap());
+  let validation_json = validation.clone();
+  let is_valid = helpers::validator::has_errors(validation);
 
   let response;
-  if results.len() == 1 {
-    let person = &results[0];
-    response = helpers::status::Response {
-      success: true,
-      code: 200,
-      data: person.to_json()
-    };
 
-    res.set(StatusCode::Ok);
+  if is_valid {
+    let connection = lib::db::establish_connection();
 
+    let input_uuid = Uuid::parse_str(req.param("uuid").unwrap()).unwrap();
+    let results = persons.filter( uuid.eq(input_uuid))
+      .load::<models::people::Person>(&connection)
+      .expect("error pulling person matching uuid");
+
+    if results.len() == 1 {
+      let person = &results[0];
+      response = helpers::status::Response {
+        success: true,
+        code: 200,
+        data: person.to_json()
+      };
+
+      res.set(StatusCode::Ok);
+
+    } else {
+      let error = helpers::status::Error {
+        error : "An error occured attempting to look up the given identifier".to_string(),
+        case: Json::from_str("{}").unwrap()
+      };
+
+      response = helpers::status::Response {
+        success: false,
+        code: 400,
+        data: error.to_json()
+      };
+
+      res.set(StatusCode::BadRequest);
+
+    }
   } else {
+    let case = format!("{:?}", validation_json);
     let error = helpers::status::Error {
-      error : "An error occured attempting to look up the given identifier".to_string(),
-      case: Json::from_str("{}").unwrap()
+      error : "There is one or more validation error".to_string(),
+      case: Json::from_str(&case).unwrap()
     };
 
     response = helpers::status::Response {
       success: false,
-      code: 400,
+      code: 412,
       data: error.to_json()
     };
-
-    res.set(StatusCode::BadRequest);
-
+    res.set(StatusCode::PreconditionFailed); 
   }
-
 
   res.set(MediaType::Json);
   res.send(response.to_json())
@@ -71,8 +100,6 @@ fn validate_put (t: &models::people::UpdatePerson, u: &str) -> BTreeMap<String, 
 pub fn put<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a> {
   use lib::schema::persons::dsl::{persons, uuid};
 
-  let connection = lib::db::establish_connection();
-
   let person = try_with!(res, {
       req.json_as::<models::people::UpdatePerson>().map_err(|e| (StatusCode::BadRequest, e))
   });
@@ -84,6 +111,8 @@ pub fn put<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a>
   let response;
 
   if is_valid {
+    let connection = lib::db::establish_connection();
+
     let input_uuid = Uuid::parse_str(req.param("uuid").unwrap()).unwrap();
     let results = persons.filter( uuid.eq(input_uuid))
       .load::<models::people::Person>(&connection)
@@ -152,8 +181,6 @@ fn validate_post (t: &models::people::NewPerson) -> BTreeMap<String, Vec<String>
 pub fn post<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a> {
   use lib::schema::persons::dsl::{persons, email};
 
-  let connection = lib::db::establish_connection();
-
   let person = try_with!(res, {
     req.json_as::<models::people::NewPerson>().map_err(|e| (StatusCode::BadRequest, e))
   });
@@ -165,6 +192,8 @@ pub fn post<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a
   let response;
 
   if is_valid {
+    let connection = lib::db::establish_connection();
+
     let results = persons.filter(email.eq(&person.email))
       .load::<models::people::Person>(&connection)
       .expect("error pulling person matching uuid");
@@ -218,37 +247,56 @@ pub fn post<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a
 
 pub fn delete<'a>(req: &mut Request, mut res: Response<'a>) -> MiddlewareResult<'a> {
   use lib::schema::persons::dsl::{persons, uuid};
-
-  let connection = lib::db::establish_connection();
-
-  let input_uuid = Uuid::parse_str(req.param("uuid").unwrap()).unwrap();
-  let result = diesel::delete(persons.filter( uuid.eq(input_uuid)))
-    .execute(&connection)
-    .expect("Error deleting person");
+  let validation: BTreeMap<String, Vec<String>> = validate_get_delete(req.param("uuid").unwrap());
+  let validation_json = validation.clone();
+  let is_valid = helpers::validator::has_errors(validation);
 
   let response;
 
-  if result == 1 {
-    let message = "The resource has been destroyed".to_string();
-    response = helpers::status::Response {
-      success: true,
-      code: 410,
-      data: message.to_json()
-    };
+  if is_valid {
+      
+    let connection = lib::db::establish_connection();
+
+    let input_uuid = Uuid::parse_str(req.param("uuid").unwrap()).unwrap();
+    let result = diesel::delete(persons.filter( uuid.eq(input_uuid)))
+      .execute(&connection)
+      .expect("Error deleting person");
+
+    if result == 1 {
+      let message = "The resource has been destroyed".to_string();
+      response = helpers::status::Response {
+        success: true,
+        code: 410,
+        data: message.to_json()
+      };
+    } else {
+      let error = helpers::status::Error {
+        error : "The resource looks to have already been destroyed".to_string(),
+        case: Json::from_str("{}").unwrap()
+      };
+
+      response = helpers::status::Response {
+        success: false,
+        code: 410,
+        data: error.to_json()
+      }
+    }
+    res.set(StatusCode::Gone);
   } else {
+    let case = format!("{:?}", validation_json);
     let error = helpers::status::Error {
-      error : "The resource looks to have already been destroyed".to_string(),
-      case: Json::from_str("{}").unwrap()
+      error : "There is one or more validation error".to_string(),
+      case: Json::from_str(&case).unwrap()
     };
 
     response = helpers::status::Response {
       success: false,
-      code: 410,
+      code: 412,
       data: error.to_json()
-    }
+    };
+    res.set(StatusCode::PreconditionFailed);
   }
 
   res.set(MediaType::Json);
-  res.set(StatusCode::Gone);
   res.send(response.to_json())
 }
